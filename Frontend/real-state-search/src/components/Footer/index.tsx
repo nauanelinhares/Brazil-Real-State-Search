9bde94466f786331b552c21131b8c61ad784bc07
nauanelinhares <nauane.linhares@gmail.com>
2024-11-17 11:31:02 -0300

    feat: add Header and Footer components

---
+++ Frontend/real-state-search/src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>Desenvolvido por Nauane Linhares</footer>;
+};
+
+export default Footer;
---
+++ Frontend/real-state-search/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Bem vindo a procura de hóteis</h1>
+    </div>
+  );
+};
+
+export default Header;
