1894f14e2919aed0f86c84bf2b689ef44f1813da
nauanelinhares <nauane.linhares@gmail.com>
2024-11-23 07:54:14 -0300

    feat: update house fetching URL

--- Frontend/real-state-search/src/api/houses.ts
+++ Frontend/real-state-search/src/api/houses.ts
@@ -1,7 +1,7 @@
 import { HouseInfo } from "./types";
 
 async function getHouseById(id: string) {
-    const response = await fetch(`http://127.0.0.1:8000/api/v1/house/${id}/`);
+    const response = await fetch(`http://127.0.0.1:8000/api/v1/house/id/${id}/`);
     console.log(response.status);
     if (!response.ok) {
         throw new Error(`Error fetching house with id ${id}: ${response.statusText}`);
