ec9897fd8c12e2b34e9e31ca9536d63eee61a964
nauanelinhares <nauane.linhares@gmail.com>
2024-11-27 14:01:42 -0300

    feat: extend house retrieval functionality with additional filters for size, rooms, bathrooms, and rent range

--- Backend/api/v1/endpoints/house.py
+++ Backend/api/v1/endpoints/house.py
@@ -10,8 +10,34 @@ router = APIRouter()
 group = "/house"
 
 @router.get("/houses", response_model=List[InfoHouse])
-def read_houses(neighborhood: str = None, created_at: str = None, company: str = None, skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
-    houses = get_houses(db, skip=skip, limit=limit, neighborhood=neighborhood, created_at=created_at, company=company)
+def read_houses(
+    neighborhood: str = None,
+    created_at: str = None,
+    company: str = None,
+    size: int = None,
+    number_rooms: int = None,
+    number_bathrooms: int = None,
+    number_parking_spaces: int = None,
+    min_rent: int = None,
+    max_rent: int = None,
+    skip: int = 0,
+    limit: int = 20,
+    db: Session = Depends(get_db)
+):
+    houses = get_houses(
+        db,
+        skip=skip,
+        limit=limit,
+        neighborhood=neighborhood,
+        created_at=created_at,
+        company=company,
+        size=size,
+        number_rooms=number_rooms,
+        number_bathrooms=number_bathrooms,
+        number_parking_spaces=number_parking_spaces,
+        min_rent=min_rent,
+        max_rent=max_rent
+    )
     return houses
 
 
--- Backend/crud/house.py
+++ Backend/crud/house.py
@@ -2,8 +2,48 @@ from sqlalchemy.orm import Session
 from sqlalchemy import func
 from database.models import InfoHouse, UrlHouse
 
-def get_houses(db: Session, skip: int = 0, limit: int = 20, neighborhood: str = None, created_at: str = None, company: str = None):
-    query = db.query( InfoHouse.id,
+def get_houses(db: Session, skip: int = 0,
+               limit: int = 20,
+               neighborhood: str = None,
+               created_at: str = None,
+               company: str = None,
+               size: int = None,
+               number_rooms: int = None,
+               number_bathrooms: int = None,
+               number_parking_spaces: int = None,
+               min_rent: int = None,
+               max_rent: int = None):
+    filters = []
+    
+    if neighborhood:
+        filters.append(InfoHouse.neighborhood == neighborhood)
+    
+    if created_at:
+        filters.append(InfoHouse.created_at >= created_at)
+    
+    if company:
+        filters.append(InfoHouse.company == company)
+    
+    if number_rooms:
+        filters.append(InfoHouse.number_rooms == number_rooms)
+    
+    if number_bathrooms:
+        filters.append(InfoHouse.number_bathrooms == number_bathrooms)
+    
+    if number_parking_spaces:
+        filters.append(InfoHouse.number_parking_spaces == number_parking_spaces)
+    
+    if size:
+        filters.append(InfoHouse.size >= size)
+        
+    if min_rent:
+        filters.append(InfoHouse.rent >= min_rent)
+    
+    if max_rent:
+        filters.append(InfoHouse.rent <= max_rent)
+    
+    query = db.query(
+        InfoHouse.id,
         InfoHouse.rent,
         InfoHouse.tax_hotel,
         InfoHouse.iptu,
@@ -18,16 +58,8 @@ def get_houses(db: Session, skip: int = 0, limit: int = 20, neighborhood: str = 
         InfoHouse.images,
         InfoHouse.created_at,
         InfoHouse.updated_at,
-        UrlHouse.url).outerjoin(UrlHouse, InfoHouse.id == UrlHouse.id)
-    
-    if neighborhood:
-        query = query.filter(InfoHouse.neighborhood == neighborhood)
-    
-    if created_at:
-        query = query.filter(InfoHouse.created_at >= created_at)
-    
-    if company:
-        query = query.filter(InfoHouse.company == company)
+        UrlHouse.url
+    ).outerjoin(UrlHouse, InfoHouse.id == UrlHouse.id).filter(*filters)
     
     return query.order_by(InfoHouse.rent).offset(skip).limit(limit).all()
 
